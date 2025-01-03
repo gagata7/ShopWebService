efc2ea593daf1cde3f9a8470139033f514ca3d4e
gagata7 <339102@uwr.edu.pl>
2025-01-03 21:00:02 +0100

    formatted all files by prettier addon

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const path = require('path');
-const methodOverride = require('method-override');
-const bcrypt = require('bcryptjs');
-const { Pool } = require('pg');
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const path = require("path");
+const methodOverride = require("method-override");
+const bcrypt = require("bcryptjs");
+const { Pool } = require("pg");
 
 const app = express();
 const port = 3000;
 
 // set ejs as view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // adding middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(methodOverride('_method'));
-app.use(session({
-  secret: 'sekret',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { 
-    maxAge: 600000 // user will be logged out after 10 minutes
-  }
-}));
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "sekret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 600000, // user will be logged out after 10 minutes
+    },
+  })
+);
 
 // postgreSQL database configuration
 const pool = new Pool({
-  user: 'admin',
-  host: 'localhost',
-  database: 'shop_webservice',
-  password: 'haslo',
+  user: "admin",
+  host: "localhost",
+  database: "shop_webservice",
+  password: "haslo",
   port: 5432,
 });
 
@@ -87,231 +89,233 @@ async function initializeDatabase() {
   `);
 }
 
-initializeDatabase().then(() => console.log('Database initialized successfully.'));
+initializeDatabase().then(() =>
+  console.log("Database initialized successfully.")
+);
 
 // index page
-app.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "views/index.html"));
 });
 
-
 // products list page
-app.get('/products', async (req, res) => {
-    try {
-        const { rows } = await pool.query('SELECT * FROM products');
-        const user = req.session.user;
-        res.render('products', { products: rows, user: user });
-    } catch (err) {
-        res.status(500).send('Error: ' + err.message);
-    }
+app.get("/products", async (req, res) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM products");
+    const user = req.session.user;
+    res.render("products", { products: rows, user: user });
+  } catch (err) {
+    res.status(500).send("Error: " + err.message);
+  }
 });
 
 // searching up a product by name or description
-app.get('/products/search', async (req, res) => {
+app.get("/products/search", async (req, res) => {
   const { query } = req.query;
   try {
     let matchingProducts = [];
     if (query) {
       const { rows } = await pool.query(
-        'SELECT * FROM products WHERE name ILIKE $1 OR description ILIKE $1',
+        "SELECT * FROM products WHERE name ILIKE $1 OR description ILIKE $1",
         [`%${query}%`]
       );
       matchingProducts = rows;
     }
-    res.render('product_search', { products: matchingProducts, searchQuery: query || '' });
+    res.render("product_search", {
+      products: matchingProducts,
+      searchQuery: query || "",
+    });
   } catch (err) {
-    res.status(500).send('Error: ' + err.message);
+    res.status(500).send("Error: " + err.message);
   }
 });
 
-
 // user registration form
-app.get('/register', async (req, res) => {
-    res.render('register');
+app.get("/register", async (req, res) => {
+  res.render("register");
 });
 
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   try {
-    await pool.query(
-      'INSERT INTO users (username, password) VALUES ($1, $2)',
-      [username, await hashPassword(password)]
-    );
-    res.status(201).send('User registered successfully.');
+    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+      username,
+      await hashPassword(password),
+    ]);
+    res.status(201).send("User registered successfully.");
   } catch (err) {
-    res.status(400).send('Error: ' + err.message);
+    res.status(400).send("Error: " + err.message);
   }
 });
 
 // user logging in form
-app.get('/login', async (req, res) => {
-    res.render('login');
+app.get("/login", async (req, res) => {
+  res.render("login");
 });
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const { rows } = await pool.query(
-    'SELECT * FROM users WHERE username = $1',
-    [username]
-  );
-  if (rows.length > 0 && await verifyPassword(password, rows[0].password)) {
+  const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [
+    username,
+  ]);
+  if (rows.length > 0 && (await verifyPassword(password, rows[0].password))) {
     req.session.user = rows[0];
-    res.redirect('/products');
+    res.redirect("/products");
   } else {
-    res.status(401).send('Invalid username or password.');
+    res.status(401).send("Invalid username or password.");
   }
 });
 
 // listing cart items
-app.get('/cart', async (req, res) => {
-    if (!req.session.user) {
-        return res.status(401).send('Content available only for logged in users.');
-    }
-    try {
-        const { rows } = await pool.query(
-            'SELECT products.*, carts.quantity FROM products JOIN carts ON products.id = carts.product_id WHERE carts.user_id = $1',
-            [req.session.user.id]
-        );
-        res.render('cart', { products: rows });
-    } catch (err) {
-        res.status(500).send('Error' + err.message);
-    }
+app.get("/cart", async (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).send("Content available only for logged in users.");
+  }
+  try {
+    const { rows } = await pool.query(
+      "SELECT products.*, carts.quantity FROM products JOIN carts ON products.id = carts.product_id WHERE carts.user_id = $1",
+      [req.session.user.id]
+    );
+    res.render("cart", { products: rows });
+  } catch (err) {
+    res.status(500).send("Error" + err.message);
+  }
 });
-
 
 // adding up an item to the cart
 
-app.post('/cart/add', async (req, res) => {
-    if (!req.session.user) {
-        return res.status(401).send('Content available only for logged in users.');
-    }
-    const { productId, quantity } = req.body;
-    try {
-      await pool.query(
-          'INSERT INTO carts (user_id, product_id, quantity) VALUES ($1, $2, $3)',
-          [req.session.user.id, productId, quantity || 1]
-      );
-      res.redirect('/cart');
-    } catch (err) {
-        res.status(400).send('Error: ' + err.message);
-    }
+app.post("/cart/add", async (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).send("Content available only for logged in users.");
+  }
+  const { productId, quantity } = req.body;
+  try {
+    await pool.query(
+      "INSERT INTO carts (user_id, product_id, quantity) VALUES ($1, $2, $3)",
+      [req.session.user.id, productId, quantity || 1]
+    );
+    res.redirect("/cart");
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
 });
 
 // removing an item from the cart
 
-app.post('/cart/remove', async (req, res) => {
-    if (!req.session.user) {
-        return res.status(401).send('Content available only for logged in users.');
-    }
-    const { productId } = req.body;
-    try {
-        await pool.query(
-            'DELETE FROM carts WHERE user_id = $1 AND product_id = $2',
-            [req.session.user.id, productId]
-        );
-        res.redirect('/cart');
-    } catch (err) {
-        res.status(400).send('Error: ' + err.message);
-    }
+app.post("/cart/remove", async (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).send("Content available only for logged in users.");
+  }
+  const { productId } = req.body;
+  try {
+    await pool.query(
+      "DELETE FROM carts WHERE user_id = $1 AND product_id = $2",
+      [req.session.user.id, productId]
+    );
+    res.redirect("/cart");
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
 });
 
 // listing all users in the database
-app.get('/users', async (req, res) => {
-    if (!req.session.user || !req.session.user.is_admin) {
-        return res.status(401).send('Content available only for privileged users.');
-    }
-    try {
-        const { rows } = await pool.query('SELECT * FROM users');
-        res.render('users', { users: rows });
-    } catch (err) {
-        res.status(500).send('Error: ' + err.message);
-    }
+app.get("/users", async (req, res) => {
+  if (!req.session.user || !req.session.user.is_admin) {
+    return res.status(401).send("Content available only for privileged users.");
+  }
+  try {
+    const { rows } = await pool.query("SELECT * FROM users");
+    res.render("users", { users: rows });
+  } catch (err) {
+    res.status(500).send("Error: " + err.message);
+  }
 });
-
 
 // adding a new product to the database
-app.get('/products/add', async (req, res) => {
+app.get("/products/add", async (req, res) => {
   if (!req.session.user || !req.session.user.is_admin) {
-    return res.status(401).send('Content available only for privileged users.');
+    return res.status(401).send("Content available only for privileged users.");
   }
-  res.render('add_product');
+  res.render("add_product");
 });
 
-app.post('/products/add', async (req, res) => {
+app.post("/products/add", async (req, res) => {
   if (!req.session.user || !req.session.user.is_admin) {
-    return res.status(401).send('Content available only for privileged users.');
+    return res.status(401).send("Content available only for privileged users.");
   }
   const { name, description, price } = req.body;
   try {
     await pool.query(
-        'INSERT INTO products (name, description, price) VALUES ($1, $2, $3)',
-        [name, description, price]
+      "INSERT INTO products (name, description, price) VALUES ($1, $2, $3)",
+      [name, description, price]
     );
-    res.redirect('/products');
+    res.redirect("/products");
   } catch (err) {
-      res.status(400).send('Error:' + err.message);
+    res.status(400).send("Error:" + err.message);
   }
 });
 
 // removing a product from the whole database
-app.get('/products/remove', async (req, res) => {
-    if (!req.session.user || !req.session.user.is_admin) {
-        return res.status(401).send('Content available only for privileged users.');
-    }
-    res.render('delete_product');
+app.get("/products/remove", async (req, res) => {
+  if (!req.session.user || !req.session.user.is_admin) {
+    return res.status(401).send("Content available only for privileged users.");
+  }
+  res.render("delete_product");
 });
 
-app.post('/products/remove', async (req, res) => {
-    if (!req.session.user || !req.session.user.is_admin) {
-        return res.status(401).send('Content available only for privileged users.');
-    }
-    const { productId } = req.body;
-    try {
-        // first, remove the product from the carts
-        await pool.query('DELETE FROM carts WHERE product_id = $1', [productId]);
+app.post("/products/remove", async (req, res) => {
+  if (!req.session.user || !req.session.user.is_admin) {
+    return res.status(401).send("Content available only for privileged users.");
+  }
+  const { productId } = req.body;
+  try {
+    // first, remove the product from the carts
+    await pool.query("DELETE FROM carts WHERE product_id = $1", [productId]);
 
-        // then, remove the product from the products table
-        await pool.query('DELETE FROM products WHERE id = $1', [productId]);
-        res.redirect('/products');
-    } catch (err) {
-        res.status(400).send('Error: ' + err.message);
-    }
+    // then, remove the product from the products table
+    await pool.query("DELETE FROM products WHERE id = $1", [productId]);
+    res.redirect("/products");
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
 });
 
 // editing a product
-app.get('/products/edit/:id', async (req, res) => {
-    if (!req.session.user || !req.session.user.is_admin) {
-      return res.status(401).send('Content available only for privileged users.');
-    }
-    const { id } = req.params;
-    try {
-        const { rows } = await pool.query('SELECT * FROM products WHERE id = $1', [id]);
-        res.render('edit_product', { product: rows[0] });
-    } catch (err) {
-        res.status(400).send('Error: ' + err.message);
-    }
+app.get("/products/edit/:id", async (req, res) => {
+  if (!req.session.user || !req.session.user.is_admin) {
+    return res.status(401).send("Content available only for privileged users.");
+  }
+  const { id } = req.params;
+  try {
+    const { rows } = await pool.query("SELECT * FROM products WHERE id = $1", [
+      id,
+    ]);
+    res.render("edit_product", { product: rows[0] });
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
 });
 
-app.put('/products/edit', async (req, res) => {
-    if (!req.session.user || !req.session.user.is_admin) {
-      return res.status(401).send('Content available only for privileged users.');
-    }
-    const { productId, name, description, price } = req.body;
-    try {
-        await pool.query(
-            'UPDATE products SET name = $1, description = $2, price = $3 WHERE id = $4',
-            [name, description, price, productId]
-        );
-        res.redirect('/products');
-    } catch (err) {
-        res.status(400).send('Error: ' + err.message);
-    }
+app.put("/products/edit", async (req, res) => {
+  if (!req.session.user || !req.session.user.is_admin) {
+    return res.status(401).send("Content available only for privileged users.");
+  }
+  const { productId, name, description, price } = req.body;
+  try {
+    await pool.query(
+      "UPDATE products SET name = $1, description = $2, price = $3 WHERE id = $4",
+      [name, description, price, productId]
+    );
+    res.redirect("/products");
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
 });
 
 // list submitted orders
-app.get('/admin/placed', async (req, res) => {
+app.get("/admin/placed", async (req, res) => {
   if (!req.session.user || !req.session.user.is_admin) {
-    return res.status(401).send('Content available only for privileged users.');
+    return res.status(401).send("Content available only for privileged users.");
   }
   try {
     const { rows } = await pool.query(
@@ -330,16 +334,16 @@ app.get('/admin/placed', async (req, res) => {
        JOIN order_items oi ON o.id = oi.order_id
        JOIN products p ON oi.product_id = p.id`
     );
-    res.render('orders', { orders: rows });
+    res.render("orders", { orders: rows });
   } catch (err) {
-    res.status(500).send('Error: ' + err.message);
+    res.status(500).send("Error: " + err.message);
   }
 });
 
 // list open orders (still in carts but not submitted)
-app.get('/admin/open', async (req, res) => {
+app.get("/admin/open", async (req, res) => {
   if (!req.session.user || !req.session.user.is_admin) {
-    return res.status(401).send('Content available only for privileged users.');
+    return res.status(401).send("Content available only for privileged users.");
   }
   try {
     const { rows } = await pool.query(
@@ -355,27 +359,27 @@ app.get('/admin/open', async (req, res) => {
         GROUP BY c.user_id, u.username
         ORDER BY user_name;`
     );
-    res.render('open_orders', { orders: rows });
+    res.render("open_orders", { orders: rows });
   } catch (err) {
-    res.status(500).send('Error: ' + err.message);
+    res.status(500).send("Error: " + err.message);
   }
 });
 
 // checkout the cart
-app.post('/checkout', async (req, res) => {
+app.post("/checkout", async (req, res) => {
   if (!req.session.user) {
-    return res.status(401).send('Content available only for logged in users.');
+    return res.status(401).send("Content available only for logged in users.");
   }
   try {
     // Check if cart is not empty
     const { rows } = await pool.query(
-      'SELECT * FROM carts WHERE user_id = $1',
+      "SELECT * FROM carts WHERE user_id = $1",
       [req.session.user.id]
     );
 
     // create new order
-    const { rows: newOrderRows} = await pool.query(
-      'INSERT INTO orders (user_id) VALUES ($1) RETURNING id',
+    const { rows: newOrderRows } = await pool.query(
+      "INSERT INTO orders (user_id) VALUES ($1) RETURNING id",
       [req.session.user.id]
     );
     const orderId = newOrderRows[0].id;
@@ -388,14 +392,15 @@ app.post('/checkout', async (req, res) => {
     );
 
     // clear the cart
-    await pool.query('DELETE FROM carts WHERE user_id = $1', [req.session.user.id]);
+    await pool.query("DELETE FROM carts WHERE user_id = $1", [
+      req.session.user.id,
+    ]);
 
-    res.redirect('/products');
+    res.redirect("/products");
   } catch (err) {
-    res.status(500).send('Error: ' + err.message);
+    res.status(500).send("Error: " + err.message);
   }
 });
-
 
 // run the server (with new, cleared session)
 app.listen(port, () => {
